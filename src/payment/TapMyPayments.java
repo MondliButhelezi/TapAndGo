aa7be04668acd91aac96db2fe39695a686c11f7b
Mondli Buthelezi <67345545+MondliButhelezi@users.noreply.github.com>
2021-05-03 09:39:12 +0200

    Add files via upload
    Adding if statement for negative balance

--- src/payment/TapMyPayments.java
+++ src/payment/TapMyPayments.java
@@ -9,8 +9,13 @@ public class TapMyPayments {
         System.out.println("You have deposited an amount of R"+ accountBalance + " to your account." );
     }
     void pay(double amountToPay){
-       accountBalance -= amountToPay;
+
+       if (amountToPay > accountBalance) {
+           System.out.println("You have insufficient funds.");
+       } else {
+           accountBalance -= amountToPay;
         System.out.println("You have paid a vendor an amount of R" + amountToPay);
+       }
     }
 
     void getBalance() {
